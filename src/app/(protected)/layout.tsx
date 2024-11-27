876f2fa85efcf4ed5f274e3a10ac832156fc8ef4
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-11-27 17:00:33 -0500

    added search with debounce

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,8 +1,8 @@
-import { Sidebar, SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { UserButton } from "@clerk/nextjs";
 import React, { FC } from "react";
 import AppSidebar from "./app-sidebar";
-import { Button } from "@/components/ui/button";
+import Search from "../_components/search";
 
 interface Props {
   children: React.ReactNode;
@@ -14,14 +14,12 @@ const SidebarLayout: FC<Props> = ({ children }) => {
       <AppSidebar />
       <main className="w-full pt-2">
         <header className="flex h-14 items-center justify-between gap-2 rounded-md border border-sidebar-border bg-sidebar p-2 px-4 shadow">
-          <input
-            type="search"
-            placeholder="Search..."
-            className="w-full max-w-[600px] rounded-full bg-gray-400/20 px-4 py-1.5 text-sm"
-          />
+          <div className="w-full">
+            <Search />
+          </div>
           <UserButton />
         </header>
-        <div className="h-3"/>
+        <div className="h-3" />
         <div className="h-[calc(100vh-5.2rem)] rounded-md border border-sidebar-border bg-sidebar shadow">
           {children}
         </div>
