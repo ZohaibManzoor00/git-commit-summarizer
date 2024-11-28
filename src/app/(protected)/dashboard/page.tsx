9fcc15b455643bbee49f5bf38d96b5a24d5a60a4
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-11-27 20:50:47 -0500

    updates

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import CommitLog from "./_components/commit.log";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { CardTitle, CardDescription } from "@/components/ui/card";
 
 const DashboardPage = () => {
   const { project } = useProject();
@@ -13,29 +14,41 @@ const DashboardPage = () => {
 
   return (
     <>
-      <div className="flex w-full flex-wrap items-center justify-between gap-y-4 px-4 pt-4">
-        <div>
-          <h1 className="text-2xl font-semibold">{project?.name}</h1>
-          <p className="ml-[.2rem] mt-1 text-sm text-muted-foreground">
-            {officialRepoName}
-          </p>
-        </div>
-        <div>
-          <Link
-            target="_blank"
-            href={project?.githubUrl ?? ""}
-            className="mb-2 mr-2 inline-flex items-center text-white/80 hover:underline"
-          >
-            <Button className="h-10 gap-2">
-              <span className="flex items-center space-x-2">
-                <Github className="h-4 w-4" />
-                <h1>View On Github</h1>
-              </span>
-              <ExternalLink className="h-3 w-3" />
-            </Button>
-          </Link>
+      <div className="flex flex-wrap items-center justify-between gap-y-4 px-4 pt-4">
+        <div className="flex w-full items-center justify-between">
+          <div className="flex w-full items-center gap-3">
+            <div className="rounded-lg bg-black p-2 text-white dark:bg-white dark:text-black">
+              <span className="text-2xl font-bold">HB</span>
+            </div>
+            <div>
+              <CardTitle className="text-xl">Homebrew</CardTitle>
+              <Link
+                target="_blank"
+                href={project?.githubUrl ?? ""}
+                className="hover:underline"
+              >
+                <CardDescription className="flex items-center gap-1">
+                  <Github className="size-4" />
+                  github.com/Homebrew/brew
+                </CardDescription>
+              </Link>
+            </div>
+            <div className="justify-end ml-auto">
+              <Button variant="outline" asChild className="hidden sm:flex">
+                <a
+                  href="https://github.com/Homebrew/brew"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Github className="mr-2 h-4 w-4" />
+                  View On Github
+                </a>
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
+      {/* </div> */}
       <div className="mt-4" />
       <CommitLog />
     </>
